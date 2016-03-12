c990fd1c1d6415704d51c541c8473ce9a50da1cd
Zac Sturgess <zac.sturgess@wearetwogether.com>
2016-03-12 19:41:59 +0000

    Handle ClearDB specially when re-writing configuration

--- bin/populate-config-from-env.php
+++ bin/populate-config-from-env.php
@@ -6,6 +6,23 @@ $placeholders = null;
 $placeholderPattern = '/\%\%([A-Z_]+)\%\%/i';
 $config = file_get_contents('../wp-config.php');
 
+// ClearDB MySQL custom handling
+$clearDb = getenv('CLEARDB_DATABASE_URL');
+
+if ($clearDb !== false) {
+    $clearDb = strstr(substr($clearDb, strpos($clearDb, '://') + 3), '?', true);
+    
+    list($userAndPass, $hostAndDb) = explode('@', $clearDb);
+    list($user, $pass) = explode(':', $userAndPass);
+    list($host, $db) = explode('/', $hostAndDb);
+    
+    $config = str_replace('%%DB_USER%%', $user, $config);
+    $config = str_replace('%%DB_PASSWORD%%', $pass, $config);
+    $config = str_replace('%%DB_HOST%%', $host, $config);
+    $config = str_replace('%%DB_NAME%%', $db, $config);
+}
+
+// Write basic config variables
 preg_match_all($placeholderPattern, $config, $placeholders);
 
 foreach ($placeholders[1] as $placeholder) {
@@ -20,4 +37,5 @@ foreach ($placeholders[1] as $placeholder) {
   }
 }
 
+// Write new config file
 file_put_contents('../wp-config.php', $config);
