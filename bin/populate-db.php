4068daf8c0bd476781dbc516938fba8f4d0e71fe
Tom Clarke <tomc@designyourcode.co.uk>
2016-08-04 20:10:50 +0100

    Adjusting db settings

--- bin/populate-db.php
+++ bin/populate-db.php
@@ -41,31 +41,17 @@
 // file_put_contents('../wp-config.php', $config);
 
 
-use Composer\Script\Event;
- 
-class HerokuEnvironment
-{
-    /**
-     * Populate Heroku environment
-     *
-     * @param Event $event Event
-     */
-    public static function populateEnvironment(Event $event)
-    {
-        $url = getenv('CLEARDB_DATABASE_URL'); // If MySQL is chosen
-        // $url = getenv('HEROKU_POSTGRESQL_IVORY_URL'); If PostgreSQL is chosen
- 
-        if ($url) {
-            $url = parse_url($url);
-            putenv("SYMFONY__DATABASE_HOST={$url['host']}");
-            putenv("SYMFONY__DATABASE_USER={$url['user']}");
-            putenv("SYMFONY__DATABASE_PASSWORD={$url['pass']}");
- 
-            $db = substr($url['path'], 1);
-            putenv("SYMFONY__DATABASE_NAME={$db}");
-        }
- 
-        $io = $event->getIO();
-        $io->write('CLEARDB_DATABASE_URL=' . getenv('CLEARDB_DATABASE_URL'));
-    }
+$url = getenv('CLEARDB_DATABASE_URL');
+
+if ($url) {
+    $url = parse_url($url);
+    putenv("SYMFONY__DATABASE_HOST={$url['host']}");
+    putenv("SYMFONY__DATABASE_USER={$url['user']}");
+    putenv("SYMFONY__DATABASE_PASSWORD={$url['pass']}");
+
+    $db = substr($url['path'], 1);
+    putenv("SYMFONY__DATABASE_NAME={$db}");
 }
+
+$io = $event->getIO();
+$io->write('CLEARDB_DATABASE_URL=' . getenv('CLEARDB_DATABASE_URL'));
