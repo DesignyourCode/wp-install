17ee04477250ba41e32ba5eefdc26fa499d9b5a1
Tom Clarke <tomc@designyourcode.io>
2017-06-05 09:40:49 +0100

    Changing the way DB vars work - #12

--- bin/populate-db.php
+++ bin/populate-db.php
@@ -2,11 +2,7 @@
 
 chdir(__DIR__);
 
-$placeholders = null;
-$placeholderPattern = '/\%\%([A-Z_]+)\%\%/i';
-$config = file_get_contents('../wp-config.php');
-
-// ClearDB MySQL custom handling
+// ClearDB
 $clearDb = getenv('CLEARDB_DATABASE_URL');
 
 if ($clearDb !== false) {
@@ -16,22 +12,11 @@ if ($clearDb !== false) {
     list($user, $pass) = explode(':', $userAndPass);
     list($host, $db) = explode('/', $hostAndDb);
     
-    $config = str_replace('%%DB_USER%%', $user, $config);
-    $config = str_replace('%%DB_PASSWORD%%', $pass, $config);
-    $config = str_replace('%%DB_HOST%%', $host, $config);
-    $config = str_replace('%%DB_NAME%%', $db, $config);
+    // $config = str_replace('%%DB_USER%%', $user, $config);
+    // $config = str_replace('%%DB_PASSWORD%%', $pass, $config);
+    // $config = str_replace('%%DB_HOST%%', $host, $config);
+    // $config = str_replace('%%DB_NAME%%', $db, $config);
 }
 
-// Write basic config variables
-preg_match_all($placeholderPattern, $config, $placeholders);
-
-foreach ($placeholders[1] as $placeholder) {
-    $env = getenv($placeholder);
-
-    if ($env !== FALSE) {
-        $config = str_replace('%%' . $placeholder . '%%', $env, $config);
-    }
-}
-
-// Write new config file
-file_put_contents('../wp-config.php', $config);
+// @todo - auto support JawsDB
+// @todo - fallback to individual fields for custom DB setup
--- wp-config.php
+++ wp-config.php
@@ -4,6 +4,7 @@
 // Load database info and local development parameters
 // ===================================================
 require(dirname( __FILE__ ) . '/vendor/autoload.php');
+require(dirname( __FILE__ ) . '/bin/populate-db.php');
 
 $parameters = dirname( __FILE__ ) . '/app/config/parameters.yml';
 
@@ -11,10 +12,10 @@ $config = spyc_load_file($parameters);
 $var = $config['parameters'];
 
 if (!file_exists($parameters)) {
-    define( 'DB_NAME', '%%DB_NAME%%' );
-    define( 'DB_USER', '%%DB_USER%%' );
-    define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
-    define( 'DB_HOST', '%%DB_HOST%%' );
+    define( 'DB_NAME', $db );
+    define( 'DB_USER', $user );
+    define( 'DB_PASSWORD', $pass );
+    define( 'DB_HOST', $host );
 } else {
     define( 'DB_NAME', $var['db_name'] );
     define( 'DB_USER', $var['db_user'] );
